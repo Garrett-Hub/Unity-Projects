675f0d95317d62a649d05032ca00fd2188854a95
Garrett-Hub <75766557+Garrett-Hub@users.noreply.github.com>
2021-05-04 12:40:53 -0600

    Update comments

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -4,35 +4,35 @@ using UnityEngine;
 
 public static class TransformExtensions
 {
+    // Returns a normalized direction between a transform and a Vector3
     public static Vector3 DirectionTo(this Transform source, Vector3 destination)
     {
-        // Returns a normalized direction between a transform and a Vector3
         return Vector3.Normalize(destination - source.position);
     }
 
+    // Find Vector3 direction between to transforms, ignoring Y coordinates
     public static Vector3 HorizontalDirectionTo(this Transform source, Transform transform)
     {
-        // Find Vector3 direction between to transforms, ignoring Y coordinates
         return new Vector3(source.position.x, transform.position.y, source.position.z)
                 - transform.position;
     }
 
+    // Returns a distance between two transforms, ignoring Y positions
     public static float HorizontalDistanceTo(this Transform source, Transform transform)
     {
-        // Returns a distance between two transforms, ignoring Y positions
         return Vector3.Distance(source.position,
                 new Vector3(transform.position.x, source.position.y, transform.position.z));
     }
 
+    // Return a signed angle between two transform forwards, ignoring Y positions
     public static float SignedHorizontalAngleTo(this Transform source, Transform transform)
     {
-        // Return a signed angle between two transform forwards, ignoring Y positions
         return Vector3.SignedAngle(source.forward.Flat(), transform.forward.Flat(), Vector3.up);
     }
 
+    // Return a signed angle between a transform's forward and another Vector3 forward
     public static float SignedHorizontalAngleTo(this Transform source, Vector3 forward)
     {
-        // Return a signed angle between a transform's forward and another Vector3 forward
         return Vector3.SignedAngle(source.forward.Flat(), forward.Flat(), Vector3.up);
     }
 }
--- Extensions/Vector3Extensions.cs
+++ Extensions/Vector3Extensions.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public static class Vector3Extensions
 {
+    // Return new Vector3 with a swapped value
     public static Vector3 With(this Vector3 source, float? x = null, float? y = null, float? z = null)
     {
-        // Return new Vector3 with a swapped value
         return new Vector3(x ?? source.x, y ?? source.y, z ?? source.z);
     }
 
+    // Return the Vector3 with y value set to 0
     public static Vector3 Flat(this Vector3 source)
     {
-        // Return the Vector3 with y value set to 0
         return new Vector3(source.x, 0, source.z);
     }
 
+    // Takes a queried Vector3 and tests if it is behind forward
     public static bool IsBehind(this Vector3 source, Vector3 forward)
     {
-        // Takes a queried Vector3 and tests if it is behind forward
         return Vector3.Dot(source, forward) < 0f;
     }
 
+    // Returns a signed angle between two Vector3 forwards ignoring y values
     public static float SignedHorizontalAngleTo(this Vector3 source, Vector3 forward)
     {
-        // Returns a signed angle between two Vector3 forwards ignoring y values
         return Vector2.SignedAngle(new Vector2(source.x, source.z), new Vector2(forward.x, forward.z));
     }
 }
